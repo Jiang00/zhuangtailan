241100a0c1e56ae69a5d5e466dbec41eb849e174
JiangFengYong <986143964@qq.com>
2017-06-13 11:52:35 +0800

    first init

---
+++ app/src/main/java/com/and/ad/title/MainActivity.java
@@ -0,0 +1,55 @@
+package com.and.ad.title;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+
+        } else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        View title = findViewById(R.id.title);
+        ViewGroup.LayoutParams linearParams = title.getLayoutParams();
+        linearParams.height = getStatusHeight();
+        title.setLayoutParams(linearParams);
+//        想设置啥色就啥色
+        title.setBackgroundColor(Color.GREEN);
+    }
+
+    public int getStatusHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+}
